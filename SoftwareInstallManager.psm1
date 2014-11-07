cc15c84a38679db19d4750de8eed1fa7c5aaaded
Adam Bertram <abertram@gmail.com>
2014-11-06 21:57:07 -0600

    Added hash functions

--- SoftwareInstallManager.psm1
+++ SoftwareInstallManager.psm1
@@ -98,7 +98,7 @@ function Write-Log {
 		if (Test-Path variable:\ScriptLogFilePath) {
 			Add-Content -Value $Line -Path $ScriptLogFilePath
 		} else {
-			Write-Verbose $Line	
+			Write-Verbose $Line
 		}
 	} catch {
 		Write-Error $_.Exception.Message
@@ -143,8 +143,8 @@ function Start-Log {
 	}
 }
 
-function Validate-IsSoftwareInstalled ($ProductName,$Version) {
-	$Params = @{'Name' = $ProductName }
+function Validate-IsSoftwareInstalled ($ProductName, $Version) {
+	$Params = @{ 'Name' = $ProductName }
 	if ($Version) {
 		$Params.Version = $Version
 	}
@@ -177,7 +177,7 @@ function Get-InstallerType ($UninstallString) {
 	} elseif ($UninstallString -imatch 'InstallShield Installation') {
 		'InstallShield'
 	} else {
-		$false	
+		$false
 	}
 }
 
@@ -211,7 +211,7 @@ function Get-InstallLocation {
 	[CmdletBinding()]
 	[OutputType([string])]
 	param (
-		[Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)]
+		[Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true)]
 		[ValidateNotNullOrEmpty()]
 		[string]$ProductName
 	)
@@ -273,10 +273,10 @@ function Import-Certificate {
 	#>
 	[CmdletBinding()]
 	param (
-		[Parameter(Mandatory=$true)]
+		[Parameter(Mandatory = $true)]
 		[ValidateSet('CurrentUser', 'LocalMachine')]
 		[string]$Location,
-		[Parameter(Mandatory=$true)]
+		[Parameter(Mandatory = $true)]
 		[ValidateScript({
 			if ($Location -eq 'CurrentUser') {
 				(Get-ChildItem Cert:\CurrentUser | select -ExpandProperty name) -contains $_
@@ -285,7 +285,7 @@ function Import-Certificate {
 			}
 		})]
 		[string]$StoreName,
-		[Parameter(Mandatory=$true)]
+		[Parameter(Mandatory = $true)]
 		[ValidateScript({ Test-Path $_ -PathType Leaf })]
 		[string]$FilePath
 	)
@@ -388,6 +388,257 @@ function Set-RegistryValueForDefaultUser {
 		}
 	} catch {
 		Write-Log -Message $_.Exception.Message -LogLevel '3'
+	}
+}
+
+function Copy-FileWithHashCheck {
+	<#
+	.SYNOPSIS
+		This function copies a file and then verifies the copy was successful by comparing the source and destination
+		file hash values.
+	.EXAMPLE
+		PS> Copy-FileWithHashCheck -SourceFilePath 'C:\Windows\file1.txt' -DestinationFolderPath '\\COMPUTER\c$\Windows\file2.txt'
+	
+		This example copied the file from C:\Windows\file1.txt to \\COMPUTER\c$\Windows and then checks the hash for the
+		source file and destination file to ensure the copy was successful.
+	.PARAMETER SourceFilePath
+		The source file path
+	.PARAMETER DestinationFolderPath
+		The destination folder path
+	.PARAMETER Force
+		Overwrite the destination file if one exists
+	#>
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true, ValueFromPipeline = $True)]
+		[ValidateScript({ Test-Path -Path $_ -PathType Leaf })]
+		[string]$SourceFilePath,
+		[Parameter(Mandatory = $true)]
+		[ValidateScript({ Test-Path -Path $_ -PathType Container })]
+		[string]$DestinationFolderPath,
+		[Parameter()]
+		[switch]$Force
+	)
+	process {
+		try {
+			$CopyParams = @{ 'Path' = $SourceFilePath; 'Destination' = $DestinationFolderPath }
+			
+			$DestinationFilePath = "$DestinationFolderPath\$($SourceFilePath | Split-Path -Leaf)"
+			if (!$Force.IsPresent -and (Test-Path $DestinationFilePath)) {
+				throw "The proposed destination file path '$DestinationFilePath' already exists."
+			} else {
+				$CopyParams.Force = $true	
+			}
+			
+			Copy-Item @CopyParams
+			$SourceHash = Get-MyFileHash -Path $SourceFilePath
+			$DestHash = Get-MyFileHash -Path "$DestinationFolderPath\$($SourceFilePath | Split-Path -Leaf)"
+			if ($SourceHash.SHA256 -ne $DestHash.SHA256) {
+				$false
+			} else {
+				$true
+			}
+		} catch {
+			Write-Log -Message $_.Exception.Message -LogLevel '3'
+			$false
+		}
+	}
+}
+
+function Get-MyFileHash {
+    <#
+        .SYNOPSIS
+            Calculates the hash on a given file based on the seleced hash algorithm.
+
+        .DESCRIPTION
+            Calculates the hash on a given file based on the seleced hash algorithm. Multiple hashing 
+            algorithms can be used with this command.
+
+        .PARAMETER Path
+            File or files that will be scanned for hashes.
+
+        .PARAMETER Algorithm
+            The type of algorithm that will be used to determine the hash of a file or files.
+            Default hash algorithm used is SHA256. More then 1 algorithm type can be used.
+            
+            Available hash algorithms:
+
+            MD5
+            SHA1
+            SHA256 (Default)
+            SHA384
+            SHA512
+            RIPEM160
+
+        .NOTES
+            Name: Get-FileHash
+            Author: Boe Prox
+            Created: 18 March 2013
+            Modified: 28 Jan 2014
+                1.1 - Fixed bug with incorrect hash when using multiple algorithms
+
+        .OUTPUTS
+            System.IO.FileInfo.Hash
+
+        .EXAMPLE
+            Get-FileHash -Path Test2.txt
+            Path                             SHA256
+            ----                             ------
+            C:\users\prox\desktop\TEST2.txt 5f8c58306e46b23ef45889494e991d6fc9244e5d78bc093f1712b0ce671acc15      
+            
+            Description
+            -----------
+            Displays the SHA256 hash for the text file.   
+
+        .EXAMPLE
+            Get-FileHash -Path .\TEST2.txt -Algorithm MD5,SHA256,RIPEMD160 | Format-List
+            Path      : C:\users\prox\desktop\TEST2.txt
+            MD5       : cb8e60205f5e8cae268af2b47a8e5a13
+            SHA256    : 5f8c58306e46b23ef45889494e991d6fc9244e5d78bc093f1712b0ce671acc15
+            RIPEMD160 : e64d1fa7b058e607319133b2aa4f69352a3fcbc3
+
+            Description
+            -----------
+            Displays MD5,SHA256 and RIPEMD160 hashes for the text file.
+
+        .EXAMPLE
+            Get-ChildItem -Filter *.exe | Get-FileHash -Algorithm MD5
+            Path                               MD5
+            ----                               ---
+            C:\users\prox\desktop\handle.exe  50c128c5b28237b3a01afbdf0e546245
+            C:\users\prox\desktop\PortQry.exe c6ac67f4076ca431acc575912c194245
+            C:\users\prox\desktop\procexp.exe b4caa7f3d726120e1b835d52fe358d3f
+            C:\users\prox\desktop\Procmon.exe 9c85f494132cc6027762d8ddf1dd5a12
+            C:\users\prox\desktop\PsExec.exe  aeee996fd3484f28e5cd85fe26b6bdcd
+            C:\users\prox\desktop\pskill.exe  b5891462c9ca5bddfe63d3bae3c14e0b
+            C:\users\prox\desktop\Tcpview.exe 485bc6763729511dcfd52ccb008f5c59
+
+            Description
+            -----------
+            Uses pipeline input from Get-ChildItem to get MD5 hashes of executables.
+
+    #>
+	[CmdletBinding()]
+	Param (
+		[Parameter(Position = 0, Mandatory = $true, ValueFromPipelineByPropertyName = $true, ValueFromPipeline = $True)]
+		[Alias("PSPath", "FullName")]
+		[string[]]$Path,
+		
+		[Parameter(Position = 1)]
+		[ValidateSet("MD5", "SHA1", "SHA256", "SHA384", "SHA512", "RIPEMD160")]
+		[string[]]$Algorithm = "SHA256"
+	)
+	Process {
+		ForEach ($item in $Path) {
+			$item = (Resolve-Path $item).ProviderPath
+			If (-Not ([uri]$item).IsAbsoluteUri) {
+				Write-Verbose ("{0} is not a full path, using current directory: {1}" -f $item, $pwd)
+				$item = (Join-Path $pwd ($item -replace "\.\\", ""))
+			}
+			If (Test-Path $item -Type Container) {
+				Write-Warning ("Cannot calculate hash for directory: {0}" -f $item)
+				Return
+			}
+			$object = New-Object PSObject -Property @{
+				Path = $item
+			}
+			#Open the Stream
+			$stream = ([IO.StreamReader]$item).BaseStream
+			foreach ($Type in $Algorithm) {
+				[string]$hash = -join ([Security.Cryptography.HashAlgorithm]::Create($Type).ComputeHash($stream) |
+				ForEach { "{0:x2}" -f $_ })
+				$null = $stream.Seek(0, 0)
+				#If multiple algorithms are used, then they will be added to existing object
+				$object = Add-Member -InputObject $Object -MemberType NoteProperty -Name $Type -Value $Hash -PassThru
+			}
+			$object.pstypenames.insert(0, 'System.IO.FileInfo.Hash')
+			#Output an object with the hash, algorithm and path
+			Write-Output $object
+			
+			#Close the stream
+			$stream.Close()
+		}
+	}
+}
+
+function Compare-FilePath {
+	<#
+	.SYNOPSIS
+		This function checks the hash of 2 files see if they are the same
+	.EXAMPLE
+		PS> Compare-FilePath -ReferencePath 'C:\Windows\file.txt' -DifferencePath '\\COMPUTER\c$\Windows\file.txt'
+	
+		This example checks to see if the file C:\Windows\file.txt is exactly the same as the file \\COMPUTER\c$\Windows\file.txt
+	.PARAMETER ReferencePath
+		The first file path to compare
+	.PARAMETER DifferencePath
+		The second file path to compare
+	#>
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory = $true)]
+		[ValidateScript({Test-Path -Path $_ -PathType Leaf })]
+		[string]$ReferenceFilePath,
+		[Parameter(Mandatory = $true)]
+		[ValidateScript({ Test-Path -Path $_ -PathType Leaf })]
+		[string]$DifferenceFilePath
+	)
+	process {
+		try {
+			$ReferenceHash = Get-MyFileHash -Path $ReferenceFilePath
+			$DifferenceHash = Get-MyFileHash -Path $DifferenceFilePath
+			if ($ReferenceHash.SHA256 -ne $DifferenceHash.SHA256) {
+				$false
+			} else {
+				$true	
+			}
+		} catch {
+			Write-Log -Message $_.Exception.Message -LogLevel '3'
+			$false
+		}
+	}
+}
+
+function Compare-FolderPath {
+	<#
+	.SYNOPSIS
+		This function checks all files inside of a folder against another folder to see if they are the same
+	.EXAMPLE
+		PS> Compare-FilePath -ReferencePath 'C:\Windows' -DifferencePath '\\COMPUTER\c$\Windows'
+	
+		This example checks to see if the contents in C:\Windows is exactly the same as the contents in \\COMPUTER\c$\Windows
+	.PARAMETER ReferencePath
+		The first folder path to compare
+	.PARAMETER DifferencePath
+		The second folder path to compare
+	#>
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory = $true)]
+		[ValidateScript({ Test-Path -Path $_ -PathType Container })]
+		[string]$ReferenceFolderPath,
+		[Parameter(Mandatory = $true)]
+		[ValidateScript({ Test-Path -Path $_ -PathType Container })]
+		[string]$DifferenceFolderPath
+	)
+	process {
+		try {
+			$ReferenceFiles = Get-ChildItem -Path $ReferenceFolderPath -Recurse | where {!$_.PsIsContainer }
+			$DifferenceFiles = Get-ChildItem -Path $DifferenceFolderPath -Recurse | where { !$_.PsIsContainer }
+			if ($ReferenceFiles.Count -ne $DifferenceFiles.Count) {
+				Write-Log -Message "Folder path '$ReferenceFolderPath' and '$DifferenceFolderPath' file counts are different" -LogLevel '2'
+				$false
+			} elseif (Compare-Object -ReferenceObject ($ReferenceFiles | Get-MyFileHash) -DifferenceObject ($DifferenceFiles | Get-MyFileHash)) {
+				Write-Log -Message "Folder path '$ReferenceFolderPath' and '$DifferenceFolderPath' file hashes are different" -LogLevel '2'
+				$false
+			} else {
+				Write-Log -Message "Folder path '$ReferenceFolderPath' and '$DifferenceFolderPath' have equal contents"
+				$true
+			}
+		} catch {
+			Write-Log -Message $_.Exception.Message -LogLevel '3'
+			$false
+		}
 	}
 }
 
