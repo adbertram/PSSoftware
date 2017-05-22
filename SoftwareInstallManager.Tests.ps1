09e4ec4428a246458fa01a2d0e338195e5f31c50
Adam Bertram <adbertram@gmail.com>
2017-05-22 18:39:08 -0500

    Added some unit tests

--- SoftwareInstallManager.Tests.ps1
+++ SoftwareInstallManager.Tests.ps1
@@ -6,6 +6,23 @@ Get-Module -Name $ThisModuleName -All | Remove-Module -Force
 Import-Module -Name $ThisModule -Force -ErrorAction Stop
 #endregion
 
+describe 'Module-level tests' {
+	
+	it 'should validate the module manifest' {
+	
+		{ Test-ModuleManifest -Path $ThisModule -ErrorAction Stop } | should not throw
+	}
+
+	it 'should pass all error-level script analyzer rules' {
+		Invoke-ScriptAnalyzer -Path $PSScriptRoot -Severity Error
+	}
+
+	it 'should have nuget.exe included' {
+		Test-Path "$PSScriptRoot\nuget.exe" | should be $true
+	}
+
+}
+
 InModuleScope SoftwareInstallManager {
 	
 }
